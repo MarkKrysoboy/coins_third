d92e91219ac1bc7dbf6e4e491f2d415746ad1282
Mark Krysoboy <heter1209@gmail.com>
2022-03-14 10:14:20 +0300

    update config

--- src/main/java/org/example/controllers/CoinsController.java
+++ src/main/java/org/example/controllers/CoinsController.java
@@ -1,4 +1,24 @@
 package org.example.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/coins")
 public class CoinsController {
+    @GetMapping("/admin/update_db")
+    public String updateDb(){
+        return "update_db";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/")
+    public String allCoins(){
+        return "coins";
+    }
 }
